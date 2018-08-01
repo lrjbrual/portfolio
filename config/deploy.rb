52ee533ebcc4d45264d94ac73d87bd58d70e9636
Ryan Jeff Brual <lrjbrual@live.com>
2018-08-01 12:13:40 +0200

    update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :application, "portfolio"
 set :repo_url, "git@github.com:lrjbrual/portfolio.git"
 
 set :deploy_to, '/home/ryanjeff/portfolio'
+set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
 
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
