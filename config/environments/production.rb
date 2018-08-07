31cae716802b2892b4f10e46d32a416ce53e0788
Ryan Jeff Brual <lrjbrual@live.com>
2018-08-07 16:22:41 +0200

    update production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,26 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+    #smtp
+    config.action_mailer.default_url_options = { host: '178.128.248.253', port: 3000 }
+    # Don't care if the mailer can't send.
+
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    user_name:      Rails.application.secrets.mail_username,
+    password:       Rails.application.secrets.mail_password,
+    domain:         'gmail.com',
+    address:        'smtp.gmail.com',
+    port:           '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
